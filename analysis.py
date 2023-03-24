c0c3acfafd66dbb9b9bf99167f67b01a778b5d20
Ian <ianlum314@gmail.com>
2023-03-24 15:49:07 -0400

    implement `most_deaths`

--- analysis.py
+++ analysis.py
@@ -1,3 +1,20 @@
 """
 Functions for analyzing League of Legends game stats
 """
+
+
+def most_deaths(player_data):
+    """
+    Finds the most number of deaths in one game and the champion that was
+    being played
+
+    Args:
+        player_data: A DataFrame holding a players stats from various matches
+
+    Returns:
+        A tuple holding an integer, representing the number of deaths, and
+        a string, representing the champion name
+    """
+    max_deaths = player_data["deaths"].max()
+    champ_name = player_data[player_data["deaths"] == max_deaths]["championName"][0]
+    return (max_deaths, champ_name)

20b3f009eda119e7f36c56675a6d385672bf69d0
Ian <ianlum314@gmail.com>
2023-03-24 16:29:27 -0400

    implement `least_cs`

--- analysis.py
+++ analysis.py
@@ -18,3 +18,23 @@ def most_deaths(player_data):
     max_deaths = player_data["deaths"].max()
     champ_name = player_data[player_data["deaths"] == max_deaths]["championName"][0]
     return (max_deaths, champ_name)
+
+
+def least_cs(player_data):
+    """
+    Finds the least cs (creep score) per minute in one game and the champion
+    that was being played
+
+    Args:
+        player_data: A DataFrame holding a players stats from various matches
+
+    Returns:
+        A tuple holding an float, represetning the cs/m, and a string,
+        representing the champion name
+    """
+    player_data["cs/m"] = (
+        player_data["neutralMinionsKilled"] + player_data["totalMinionsKilled"]
+    ) / (player_data["timePlayed"] / 60)
+    min_cs = player_data[player_data["individualPosition"] != "UTILITY"]["cs/m"].min()
+    champ_name = player_data[player_data["cs/m"] == min_cs]["championName"][0]
+    return (min_cs, champ_name)

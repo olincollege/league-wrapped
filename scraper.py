9de619fc15a089d08c5295b9e67a42457acace13
Ian <ianlum314@gmail.com>
2023-03-24 11:50:22 -0400

    implement function to find all matches in season 12

--- scraper.py
+++ scraper.py
@@ -8,6 +8,8 @@ import pandas as pd
 from time import time
 
 YEAR_SECONDS = 31536000
+S12_START = 1641531600  # 1/07/2022, 00:00:00
+S12_END = 1668488399  # 11/14/2022, 23:59:59
 
 
 def create_watcher(filepath):
@@ -24,6 +26,45 @@ def create_watcher(filepath):
     with open(filepath, "r", encoding="UTF=8") as file:
         api_key = file.readline()
     return LolWatcher(api_key)
+
+
+def get_season_matchlist(watcher, summoner_name, region):
+    """
+    Create a list of match ids for all matches played by a summoner in
+    Season 12. The match ids are ordered from most recent to least recent.
+
+    Args:
+        watcher: A LolWatcher object holding an API key
+        summoner_name: A string representing the name of the summoner who's
+            matches will be found
+        region: A string representing the region of the summoner
+            regions can be found here: https://developer.riotgames.com/docs/lol
+            under "platform routing values"
+
+    Returns:
+        A list of strings representing the match ids
+    """
+    summoner = watcher.summoner.by_name(region, summoner_name)
+
+    matchlist = []
+    start_idx = 0
+
+    while True:
+        new_matchlist = watcher.match.matchlist_by_puuid(
+            region=region,
+            puuid=summoner["puuid"],
+            start=start_idx,
+            count=100,
+            start_time=S12_START,
+            end_time=S12_END,
+        )
+        matchlist += new_matchlist
+
+        if len(new_matchlist) != 100:
+            break
+
+        start_idx += 100
+    return matchlist
 
 
 def get_matchlist(watcher, summoner_name, region, match_count):

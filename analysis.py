8882f09226136363bff398d1dae4d30e6bd3b448
Ian <ianlum314@gmail.com>
2023-03-25 00:37:33 -0400

    implement `worst_winrate`

--- analysis.py
+++ analysis.py
@@ -9,7 +9,7 @@ def most_deaths(player_data):
     being played
 
     Args:
-        player_data: A DataFrame holding a players stats from various matches
+        player_data: A DataFrame holding a player's stats from various matches
 
     Returns:
         A tuple holding an integer, representing the number of deaths, and
@@ -26,7 +26,7 @@ def least_cs(player_data):
     that was being played
 
     Args:
-        player_data: A DataFrame holding a players stats from various matches
+        player_data: A DataFrame holding a player's stats from various matches
 
     Returns:
         A tuple holding an float, represetning the cs/m, and a string,
@@ -35,6 +35,45 @@ def least_cs(player_data):
     player_data["cs/m"] = (
         player_data["neutralMinionsKilled"] + player_data["totalMinionsKilled"]
     ) / (player_data["timePlayed"] / 60)
-    min_cs = player_data[player_data["individualPosition"] != "UTILITY"]["cs/m"].min()
+    min_cs = player_data[
+        (player_data["individualPosition"] != "UTILITY")
+        & (player_data["role"] != "SUPPORT")
+        & (player_data["teamPosition"] != "UTILITY")
+    ]["cs/m"].min()
     champ_name = player_data[player_data["cs/m"] == min_cs]["championName"][0]
     return (min_cs, champ_name)
+
+
+def worst_winrate(player_data):
+    """
+    Finds the five champions, that have more than five games played,
+    with the worst winrates
+
+    Args:
+        player_data: A DataFrame holding a player's stats from various matches
+
+    Returns:
+        A list of dictionaries representing the five champions with the worst
+        winrates. Each dictionary holds the champion's name, winrate, and
+        number of games played
+    """
+    champs_played = player_data["championName"].unique()
+
+    champ_wrs = []
+
+    for champ in champs_played:
+        champ_games = player_data[player_data["championName"] == champ]["win"]
+        games_played = len(champ_games)
+        if games_played < 5:
+            continue
+        winrate = sum(champ_games) / games_played
+
+        champ_wrs.append(
+            {
+                "champ": champ,
+                "winrate": winrate,
+                "games_played": games_played,
+            }
+        )
+
+    return sorted(champ_wrs, key=lambda champ_dict: champ_dict["wr"])[:5]

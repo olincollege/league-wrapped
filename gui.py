930f6be7a0aeda048beb9595300275a10a269a00
Moampane <mo.ampane@gmail.com>
2023-03-26 23:16:10 -0400

    Make function for worst winrates gui

--- gui.py
+++ gui.py
@@ -115,7 +115,7 @@ def make_kda_wrap(kda_data):
     KDAs = []
 
     for kda in kda_data:
-        kda["kda"] = round(kda["kda"], 2)
+        kda["kda"] = round(kda["kda"] * 10) / 10
         KDAs.append(kda["kda"])
 
     kda_texts = {}
@@ -133,5 +133,107 @@ def make_kda_wrap(kda_data):
         kda_texts[f"{i}"] = KDA_text
         if i == 4:
             canvas.itemconfig(KDA_text, fill=YELLOW)
+
+    window.mainloop()
+
+
+def make_wr_wrap(wr_data):
+    # Colors
+    BLACK = "#000000"
+
+    # Set window
+    window = t.Tk()
+    window.title("League Wrapped Worst Winrates Example")
+
+    # Make canvas
+    scale = 0.75
+    cwidth = 720 * scale
+    cheight = 1280 * scale
+    canvas = t.Canvas(width=cwidth, height=cheight)
+    canvas.grid(row=0, column=0)
+
+    # Add background
+    pilImage = Image.open("assets/TEMPLATE_winrate.jpg")
+    pilImage = pilImage.resize((int(cwidth), int(cheight)))
+    bgimg = ImageTk.PhotoImage(pilImage)
+    canvas.create_image(cwidth / 2, cheight / 2, image=bgimg)
+
+    # Add champ images
+    horrible_wr_champs = []
+    for wr_dic in wr_data:
+        horrible_wr_champs.append(wr_dic["champ"])
+
+    y = 133
+    hwr_images = {}
+
+    for champ in horrible_wr_champs:
+        image_url = (
+            f"http://ddragon.leagueoflegends.com/cdn/13.6.1/img/champion/{champ}.png"
+        )
+        r = requests.get(image_url)
+        pilImage = Image.open(BytesIO(r.content))
+        pilImage = Image.open(BytesIO(r.content)).resize(
+            (int(200 * scale), int(200 * scale))
+        )
+        horrible_wr_image = ImageTk.PhotoImage(pilImage)
+        hwr_images[f"{champ}"] = horrible_wr_image
+        canvas.create_image(
+            199 * scale, y * scale, anchor=t.NW, image=horrible_wr_image
+        )
+        y += 210
+
+    # Add winrate text
+    ywrtext = 175
+    winrates = []
+    for wr_dic in wr_data:
+        winrates.append(round(wr_dic["winrate"] * 100))
+    wr_texts = {}
+    for i in range(len(horrible_wr_champs)):
+        wr_text = canvas.create_text(
+            cwidth / 4,
+            ywrtext,
+            width=cwidth * 0.7,
+            text=f"{winrates[i]}%",
+            fill=BLACK,
+            font=("Arial", 40, "bold"),
+            anchor="e",
+        )
+        ywrtext += 157
+        wr_texts[f"{i}"] = wr_text
+
+    # Add champ text
+    ywrtext = 160
+    champ_texts = {}
+    for i in range(len(horrible_wr_champs)):
+        wr_text = canvas.create_text(
+            3 * cwidth / 5,
+            ywrtext,
+            width=cwidth * 0.7,
+            text=f"{horrible_wr_champs[i]}",
+            fill=BLACK,
+            font=("Arial", 16, "bold"),
+            anchor="w",
+        )
+        ywrtext += 157
+        champ_texts[f"{i}"] = wr_text
+
+    # Add games played text
+    ywrtext = 190
+    games_played = []
+    for wr_dic in wr_data:
+        games_played.append(wr_dic["games_played"])
+    games_texts = {}
+    for i in range(len(horrible_wr_champs)):
+        games_text = canvas.create_text(
+            3 * cwidth / 5,
+            ywrtext,
+            width=cwidth * 0.7,
+            text=f"{games_played[i]} games played",
+            fill=BLACK,
+            font=("Arial", 14),
+            anchor="w",
+        )
+        ywrtext += 157
+        games_texts[f"{i}"] = games_text
 
     window.mainloop()

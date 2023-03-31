a07b376fd9224e0f912578634c1fc186374c409a
Ian <ianlum314@gmail.com>
2023-03-30 20:51:01 -0400

    create test for api key validity

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -3,6 +3,13 @@ Check that scraper functions are setup properly
 """
 
 import os
+import sys
+from requests.exceptions import HTTPError
+
+sys.path.append("./modules")
+
+# pylint: disable=import-error, wrong-import-position
+from scraper import create_watcher
 
 
 def test_key_file_exists():
@@ -10,3 +17,13 @@ def test_key_file_exists():
     Test `key.txt` exists
     """
     assert os.path.exists("key.txt")
+
+
+def test_key_validity():
+    """
+    Test api key is valid
+    """
+    try:
+        create_watcher().champion.rotations("na1")
+    except HTTPError:
+        assert False
